2f3a2d9096e1f3adbd97874c5ff6b5d4639393de
David Richards <rawdod@gmail.com>
2007-05-22 22:09:13 -0400

    initial commit woot

---
+++ radiobar.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# xmms2 radiobar will be a simple toolbar designed for the 
+# simple needs of radio dj's that do not make sense in
+# any other client, it is intended as a supplimentary client
+# you would use other clients for playlist management
+# controls: play/pause stop, single/cont  prev/next buttons
+# flash play button when 30~ secs from stopping (colored buttons)
+# show time remaining, show current track and next track ?
+# voiceover button
+
+require 'gtk2'
+require "xmmsclient"
+require 'xmmsclient_glib'
+
+
+
+
+
