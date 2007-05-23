84921982aba20a3b50e55c6f7e17435d5d5bc74a
David Richards <rawdod@gmail.com>
2007-05-22 22:31:37 -0400

    buttons and radiobar class

--- radiobar.rb
+++ radiobar.rb
@@ -13,6 +13,44 @@ require 'gtk2'
 require "xmmsclient"
 require 'xmmsclient_glib'
 
+VERSION = 0.2
+FLASH_TIME = 30
+
+class Radiobar
+
+ def initialize 
+   @window = Gtk::Window.new
+   @window.title = "radiobar"
+   @window.border_width = 30
+   @bar = Gtk::HBox.new(false, 0)
+        
+   @playpause_button = Gtk::Button.new("Play")
+   @stop_button = Gtk::Button.new("Stop")
+   @prev_button = Gtk::Button.new("Prev")
+   @next_button = Gtk::Button.new("Next")
+   @singlecont_button = Gtk::Button.new("Cont")
+   @voice_button = Gtk::Button.new("Voice")
+
+   @bar.pack_start(@playpause_button, true, true, 0)
+   @bar.pack_start(@stop_button, true, true, 0)
+   @bar.pack_start(@prev_button, true, true, 0)
+   @bar.pack_start(@next_button, true, true, 0)
+   @bar.pack_start(@singlecont_button, true, true, 0)
+   @bar.pack_start(@voice_button, true, true, 0)
+
+   @window.add(@bar)
+   @window.signal_connect('delete_event') do
+    Gtk.main_quit
+    false
+   end
+   @window.show_all
+   Gtk.main
+ end 
+
+end
+
+
+radiobar = Radiobar.new
 
 
 
